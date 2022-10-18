7518ce49cc461ce5236552bf326daff6ef668415
unknown <rakkakeren@gmail.com>
2022-10-18 20:41:31 +0700

    change: specify pw1 and pw2 on register form

--- quiz/forms.py
+++ quiz/forms.py
@@ -1,17 +1,15 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 
 
-class LoginForm(forms.ModelForm):
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput())
+
+
+class RegisterForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ["username", "password"]
-        widgets = {"password": forms.PasswordInput()}
-
-
-class RegisterForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ["username", "password", "first_name", "last_name"]
-        widgets = {"password": forms.PasswordInput()}
+        fields = ["username", "password1", "password2", "first_name", "last_name"]
