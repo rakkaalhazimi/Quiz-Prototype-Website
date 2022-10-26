8f3fdf1ed04b28390e0d3ab686115c9799bc66f5
unknown <rakkakeren@gmail.com>
2022-10-26 13:22:42 +0700

    change: remove "id" suffix from foreign key

--- quiz/models.py
+++ quiz/models.py
@@ -29,13 +29,13 @@ class Answers(models.Model):
     choice = models.CharField(max_length=1, choices=ANSWER_CHOICES)
     answer = models.CharField(max_length=255)
     is_right = models.BooleanField(default=False)
-    question_id = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.question_id} - {self.choice} - {self.answer}"
+        return f"{self.question} - {self.choice} - {self.answer}"
 
 
 class UsersAnswers(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    question_id = models.ForeignKey(Questions, on_delete=models.CASCADE)
-    answer_id = models.ForeignKey(Answers, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answers, on_delete=models.CASCADE)
