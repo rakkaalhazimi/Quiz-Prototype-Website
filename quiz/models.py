c70abf81956ab2bd1c7805873ba3ef6e448c5d2d
unknown <rakkakeren@gmail.com>
2022-10-26 10:12:11 +0700

    change: add model repr and constant

--- quiz/models.py
+++ quiz/models.py
@@ -4,24 +4,35 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
+MAX_QUESTIONS = 10
+ANSWER_CHOICES = (
+    ("A", "A"),
+    ("B", "B"),
+    ("C", "C"),
+    ("D", "D"),
+)
+
 class Questions(models.Model):
-    max_questions = 10
     question_number = models.IntegerField(
         default=1, 
         validators=[
-            MaxValueValidator(max_questions), 
+            MaxValueValidator(MAX_QUESTIONS), 
             MinValueValidator(1)
         ])
     question = models.TextField()
+
+    def __str__(self):
+        return self.question
     
 
 class Answers(models.Model):
-    options = ["A", "B", "C", "D"]
-    option_choices = [(opt, opt) for opt in options]
-    choice = models.CharField(max_length=1, choices=option_choices)
+    choice = models.CharField(max_length=1, choices=ANSWER_CHOICES)
     answer = models.CharField(max_length=255)
     is_right = models.BooleanField(default=False)
     question_id = models.ForeignKey(Questions, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.question_id} - {self.choice} - {self.answer}"
 
 
 class UsersAnswers(models.Model):
