7de2edee35b288ef8b33086bfff187e0a151091f
unknown <rakkakeren@gmail.com>
2022-10-26 09:18:55 +0700

    change: view based on model

--- quiz/views.py
+++ quiz/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import render, redirect
 from django.utils.decorators import method_decorator
 from django.views import generic
 
-from .forms import LoginForm, RegisterForm
-from .models import Questions, Scores
+from .forms import LoginForm, RegisterForm, formset
+from .models import Questions, Answers, UsersAnswers
 
 
 # Create your views here.
@@ -14,23 +14,30 @@ class IndexView(generic.View):
 
     @method_decorator(login_required(login_url="/login"), name="dispatch")
     def get(self, request):
-        data = Questions.get_questions_answers()
-        context = {"data": data}
+        questions = Questions.objects.all()
+        questions_answers = []
+        for question in questions:
+            questions_answers.append((question, Answers.objects.filter(question_id=question.id)))
+        
+        context = {"data": questions_answers}
         return render(request, self.template_name, context)
 
     def post(self, request):
-        self.count_score(request)
+        score = 0
+        for question_id, choice in request.POST.items():
+            if question_id != "csrfmiddlewaretoken":
+                answer = Answers.objects.get(question_id=question_id, choice=choice)
+                if answer.is_right:
+                    score += 5
+        print(score)
         return redirect("board")
 
-    def count_score(self, request):
-        user_score = Scores.create_or_get_score(request.user)
-        user_score.set_score(
-            question_ids=request.POST.keys(), options=request.POST.values())
 
-
-class LeaderBoardView(generic.ListView):
-    model = Scores
+class LeaderBoardView(generic.View):
     template_name = "leader_board.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
 
     def get_queryset(self):
         return self.model.objects.order_by("-score")
