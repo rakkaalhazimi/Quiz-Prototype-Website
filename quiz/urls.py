876eba3854b8d5ed12ce6ec11f1dd7ee36c8e209
unknown <rakkakeren@gmail.com>
2022-10-30 11:44:14 +0700

    add: quiz_result view to url

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import IndexView, LeaderBoardView
+from .views import IndexView, LeaderBoardView, QuizResultView
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("board", LeaderBoardView.as_view(), name="board"),
+    path("result", QuizResultView.as_view(), name="result"),
 ]
