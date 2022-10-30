911c60ad38b095cc60018df3e3cf74cca69186ee
unknown <rakkakeren@gmail.com>
2022-10-30 08:03:12 +0700

    add: answers inline in questions admin site

--- quiz/admin.py
+++ quiz/admin.py
@@ -3,9 +3,15 @@ from django.contrib import admin
 from .models import Questions, Answers
 
 # Register your models here.
+class AnswersInline(admin.TabularInline):
+    model = Answers
+
 class QuestionsAdmin(admin.ModelAdmin):
     list_display = ("question_number", "question")
     ordering = ["question_number"]
+    inlines = [
+        AnswersInline,
+    ]
+    
 
 admin.site.register(Questions, QuestionsAdmin)
-admin.site.register(Answers)
