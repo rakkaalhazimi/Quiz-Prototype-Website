43a25c8d33fc51012cc3af2a8ed2b4ca15cdddce
unknown <rakkakeren@gmail.com>
2022-11-05 22:55:20 +0700

    add: password_reset url and path

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.urls import path, include
 
 from .views import LoginView, RegisterView, LogoutView
 
@@ -6,4 +7,24 @@ urlpatterns = [
     path("login", LoginView.as_view(), name="login"),
     path("register", RegisterView.as_view(), name="register"),
     path("logout", LogoutView.as_view(), name="logout"),
+    
+    path(
+        'password_reset', 
+        auth_views.PasswordResetView.as_view(template_name="password_reset.html"), 
+        name="password_reset"
+    ),
+    path(
+        'accounts/password_reset/done', 
+        auth_views.PasswordResetDoneView.as_view(), 
+        name="password_reset_done"
+    ),
+    path(
+        'accounts/reset/<uidb64>/<token>', 
+        auth_views.PasswordResetConfirmView.as_view(), 
+        name="password_reset_confirm"
+    ),
+    path(
+        'accounts/reset/done', 
+        auth_views.PasswordResetCompleteView.as_view(), 
+        name="password_reset_complete"),
 ]
