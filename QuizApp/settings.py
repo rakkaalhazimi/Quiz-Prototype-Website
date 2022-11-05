bf3d681123e5d2e556c415f47f12d4c6bc768595
unknown <rakkakeren@gmail.com>
2022-11-05 22:54:20 +0700

    add: email backend on std out

--- QuizApp/settings.py
+++ QuizApp/settings.py
@@ -123,3 +123,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Email Backend
+# https://docs.djangoproject.com/en/4.1/topics/email/
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
